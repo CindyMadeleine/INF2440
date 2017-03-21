471165cf539c7ca1170bfe6df3340bdd9a7b6d61
CindyMadeleine <cindymc@student.matnat.uio.no>
2017-03-21 09:24:32 +0100

    Delete multisortthreadtask.java

--- Oblig3/First try/multisortthreadtask.java
+++
@@ -1,7 +0,0 @@
-public interface multisortthreadtask {
-	public void run();
-
-	void countFrequencyofEachRadixValue(int[] arr, int shift, int mask);
-	void moveNumbersInSortedOrder(int[] arrA, int[] arrB, int shift, int mask);
-	void addUpInCount(int acumVal, int mask);
-}
